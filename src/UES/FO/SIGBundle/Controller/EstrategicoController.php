ed06b714a543302aab262900fe3c1405287dedc7
ibrahim79 <nuresamuel@live.com>
2014-05-26 15:19:05 -0600

    soy yo suncin quien hizo este volado. EstrategicoCOntroler y demas(4).

--- src/UES/FO/SIGBundle/Controller/EstrategicoController.php
+++ src/UES/FO/SIGBundle/Controller/EstrategicoController.php
@@ -7,6 +7,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Ps\PdfBundle\Annotation\Pdf;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @Route("/estrategico")
@@ -81,24 +83,47 @@ class EstrategicoController extends Controller
 
     /**
      * @Route("/asistencias", name="asistencias")
+     * @Method("GET")
      * @Template()
      */
     public function asistenciaAction()
     {
+        $form = $this->createReportForm();
+        return array('form'=> $form->createView());
+    }
+    /**
+     * @Route("/asistencias", name="asistencias_post")
+     * @Method("POST")
+     * @Template("SIGBundle:Estrategico:asistencia.html.twig")
+     */
+    public function asistenciaPostAction(Request $request)
+    {
+
+        $form = $this->createReportForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+            return $this->redirect($this->generateUrl('reporte-asistencias', array('desde' => date_format($data['desde'], 'd-m-y'), 'hasta'=>date_format($data['hasta'], 'd-m-y'), '_format'=>'pdf')));
+        }
+
+        return array('form'=> $form->createView());
     }
 
     /**
      * @Route(
-     *     "/asistencias.{_format}",
+     *     "/{desde}/{hasta}/asistencias.{_format}",
      *     name="reporte-asistencias",
      *     options={"expose"=true}
      * )
-     * @Method("POST")
+     * @Method("GET")
      * @Template()
      * @Pdf()
      */
-    public function reporteAsistenciaAction($request)
+    public function reporteAsistenciaAction(\Datetime $desde, \Datetime $hasta)
     {
+        return array('desde' => $desde, 'hasta'=> $hasta);
     }
 
     /**
@@ -121,5 +146,36 @@ class EstrategicoController extends Controller
      */
     public function reporteCasosAction($request)
     {
+    }
+
+    private function createReportForm(){
+        $constraints = array(
+            new Assert\NotBlank(array(
+                'message' => 'El campo no puede quedar vacio'
+            )));
+        $formBuilder = $this->createFormBuilder()
+            ->add('desde', 'date', array(
+                'label'       => 'Fecha de inicio',
+                'widget'=>'single_text',
+                'format'=>'d-M-y',
+                'attr'        => array('placeholder' => 'Escriba la fecha de inicio de el periodo','class'=>'datepicker'),
+                'constraints' => $constraints
+                ))
+            ->add('hasta', 'date', array(
+                'label'       => 'Fecha de fin',
+                'widget'=>'single_text',
+                'format'=>'d-M-y',
+                'attr'        => array('placeholder' => 'Escriba la fecha de fin de el periodo','class'=>'datepicker'),
+                'constraints' => $constraints
+                ))
+            ->add('actions', 'form_actions');
+                $formBuilder->get('actions')
+                    ->add('Enviar', 'submit')
+                    ->add('limpiar', 'reset');
+                    $formBuilder
+                ->setAction($this->generateUrl('asistencias_post'))
+                ->setMethod('POST');
+
+        return $formBuilder->getForm();;
     }
 }
